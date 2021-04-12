6cc55303bb3c0e32ef5ee56fc77d737d0f4b98fd
R Descomplicado <paulamacaira@puc-rio.br>
2021-04-12 19:04:19 -0300

    12/04

---
+++ post_12-04.R
@@ -0,0 +1,28 @@
+# post 12/04
+
+install.packages("tidyverse")
+library(tidyverse)
+
+starwars %>%
+  filter(height > 160 |
+           eye_color == "blue")
+starwars %>%
+  arrange(height, -mass)
+
+starwars %>%
+  select(name, hair_color)
+
+starwars %>%
+  mutate(Pes = height*0.0328084)
+
+starwars %>%
+  summarise(n_distinct(species))
+
+starwars %>% 
+  group_by(gender) %>%
+  summarise(Media = mean(height))
+
+# EXERCICIO
+# Obtenha como resultado um conjunto de dados, em ordem decrescente,
+# que contenha os valores máximos por cor de olho (eye_color) dos
+# moradores do mundo (homeworld) Tatooine.
